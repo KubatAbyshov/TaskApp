697578f76a5a0e374d4460844e4c6264be5901e8
KubatAbyshov <flimdeweack@gmail.com>
2019-12-05 23:22:01 +0600

    HomeWork2

--- app/src/main/java/com/geektech/taskapp/onboard/OnBoardActivity.java
+++ app/src/main/java/com/geektech/taskapp/onboard/OnBoardActivity.java
@@ -6,19 +6,37 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.geektech.taskapp.MainActivity;
 import com.geektech.taskapp.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class OnBoardActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_board);
+
         ViewPager viewPager = findViewById(R.id.viewPager);
         SectionPagerAdapter adapter = new SectionPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager, true);
+
+        Button button = findViewById(R.id.skip);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(OnBoardActivity.this, MainActivity.class));
+            }
+        });
 
     }
 
